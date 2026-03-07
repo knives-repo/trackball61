33b9a60723c76e40e35394168ca5f3a1cee63b4a
knives-repo <kevsperanza@gmail.com>
2026-03-07 23:25:25 +0000

    Change KC_GESC to KC_ESC in keymap.c

--- trackballseries/61ctc/keymaps/vial/keymap.c
+++ trackballseries/61ctc/keymaps/vial/keymap.c
@@ -124,7 +124,7 @@ bool get_speculative_hold(uint16_t keycode, keyrecord_t* record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_MAC] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮               ╭──────────────────────────────────────────────────────╮
-        KC_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
+        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
   // ├──────────────────────────────────────────────────────┤               ├──────────────────────────────────────────────────────┤
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤               ├──────────────────────────────────────────────────────┤
