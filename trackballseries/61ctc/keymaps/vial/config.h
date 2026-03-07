15ed6271b76e9b4dce87f6c07bdeb2c0cc76c092
knives-repo <kevsperanza@gmail.com>
2026-03-07 16:36:12 +0000

    Define FLOW_TAP_TERM and SPECULATIVE_HOLD
    Add FLOW_TAP_TERM and SPECULATIVE_HOLD definitions.

--- trackballseries/61ctc/keymaps/vial/config.h
+++ trackballseries/61ctc/keymaps/vial/config.h
@@ -32,6 +32,8 @@
 // #define TAPPING_TERM_PER_KEY
 #define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#define FLOW_TAP_TERM 75
+#define SPECULATIVE_HOLD
 
 /* Charybdis-specific features. */
 #ifdef POINTING_DEVICE_ENABLE
