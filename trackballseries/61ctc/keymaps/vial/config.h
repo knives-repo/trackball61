88ef52a118225f1ff37b3d37c37b0b55a72a9518
knives-repo <kevsperanza@gmail.com>
2026-03-08 00:21:27 +0000

    Enable per-key tapping term configuration

--- trackballseries/61ctc/keymaps/vial/config.h
+++ trackballseries/61ctc/keymaps/vial/config.h
@@ -29,7 +29,7 @@
 
 /** Tap-hold configuration */
 #define TAPPING_TERM 200
-// #define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM_PER_KEY
 #define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 #define FLOW_TAP_TERM 75
