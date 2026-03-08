b6ef48d6f88ea8bad22cacdb6e7a67531141d24f
knives-repo <kevsperanza@gmail.com>
2026-03-08 00:58:05 +0000

    Enable NKRO by default in config.h

--- trackballseries/61ctc/keymaps/vial/config.h
+++ trackballseries/61ctc/keymaps/vial/config.h
@@ -27,6 +27,9 @@
 #endif // VIAL_ENABLE
 #endif     // VIA_ENABLE
 
+/** other config */
+#define NKRO_DEFAULT_ON true
+
 /** Tap-hold configuration */
 #define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
