48e3beeab86365bee43fe4fe89317c78ad949b9c
knives-repo <kevsperanza@gmail.com>
2026-03-10 23:36:28 +0000

    Update keymap.c

--- trackballseries/61ctc/keymaps/vial/keymap.c
+++ trackballseries/61ctc/keymaps/vial/keymap.c
@@ -655,7 +655,7 @@ if (last_input_activity_elapsed() > OLED_TIMEOUT) {
 #    define MIN_RUN_SPEED       40
 
 /* advanced settings */
-#    define ANIM_FRAME_DURATION 150  // how long each frame lasts in ms
+#    define ANIM_FRAME_DURATION 200  // how long each frame lasts in ms
 #    define ANIM_SIZE1           96   // number of bytes in array. If you change sprites, minimize for adequate firmware size. max is 1024
 
 /* timers */
