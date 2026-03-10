7a4103d2f8e0fb7a80a2fbedde92499d5bac54af
knives-repo <kevsperanza@gmail.com>
2026-03-10 23:08:21 +0000

    Update keymap.c

--- trackballseries/61ctc/keymaps/vial/keymap.c
+++ trackballseries/61ctc/keymaps/vial/keymap.c
@@ -438,7 +438,7 @@ uint16_t startup_timer = 0;
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
      if (is_keyboard_master()){
-        return OLED_ROTATION_270;
+        return rotation;
     }else{
         return rotation; //OLED_ROTATION_270;
         }
