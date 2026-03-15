b9399f30449c4983ec60d856e1b29149d499d1ba
knives-repo <kevsperanza@gmail.com>
2026-03-15 01:25:09 +0000

    Update keymap.c

--- trackballseries/61ctc/keymaps/vial/keymap.c
+++ trackballseries/61ctc/keymaps/vial/keymap.c
@@ -375,7 +375,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
             return OLED_ROTATION_270;  // Ocean Dream Mode
         }
     } else {
-        return OLED_ROTATION_270;
+        return OLED_ROTATION_0;
         }
 }
 
