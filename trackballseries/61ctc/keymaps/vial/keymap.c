528cd6a8f3813590dd43bdbc7f2e5f132d59fa34
knives-repo <kevsperanza@gmail.com>
2026-03-18 21:34:51 +0000

    Update keymap.c

--- trackballseries/61ctc/keymaps/vial/keymap.c
+++ trackballseries/61ctc/keymaps/vial/keymap.c
@@ -127,11 +127,9 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 bool get_speculative_hold(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) { // These keys may be speculatively held.
         case LGUI_T(KC_SPACE):
-        case LGUI_T(KC_DELETE):
         case LSFT_T(KC_BSLS):
         case LCTL_T(KC_DELETE):
         case LCTL_T(KC_SPACE):
-        case LALT_T(KC_BSPC):
         case LT(4, KC_ENTER):
         case LT(5, KC_SCLN):
             return true;
@@ -143,15 +141,15 @@ bool get_speculative_hold(uint16_t keycode, keyrecord_t* record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_MAC] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮                           ╭──────────────────────────────────────────────────────╮
-MT(MOD_LCTL | MOD_LSFT | MOD_LALT | MOD_LGUI, KC_ESC), KC_1, KC_2, KC_3,KC_4, KC_5, KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+MT(MOD_LCTL | MOD_LSFT | MOD_LGUI, KC_ESC), KC_1, KC_2, KC_3,KC_4, KC_5, KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   // ├──────────────────────────────────────────────────────┤                           ├──────────────────────────────────────────────────────┤
 MT(MOD_LCTL | MOD_LGUI | MOD_LALT, KC_TAB),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   // ├──────────────────────────────────────────────────────┤                           ├──────────────────────────────────────────────────────┤
        KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, LT(5, KC_SCLN), KC_QUOT,
   // ├──────────────────────────────────────────────────────┤                           ├──────────────────────────────────────────────────────┤
-       LSFT_T(KC_BSLS), LT(3, KC_Z), KC_X, KC_C, KC_V, KC_B, LCTL_T(KC_DELETE), RCTL_T(KC_DELETE), KC_N, KC_M, KC_COMM, KC_DOT, LT(3, KC_SLSH), RSFT_T(KC_EQUAL),
+       LSFT_T(KC_BSLS), LT(3, KC_Z), KC_X, KC_C, KC_V, KC_B, LALT_T(KC_BSPC), RCTL_T(KC_DELETE), KC_N, KC_M, KC_COMM, KC_DOT, LT(3, KC_SLSH), RSFT_T(KC_EQUAL),
   // ╰──────────────────────────────────────────────────────┤                           ├──────────────────────────────────────────────────────╯
-       KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_GRV, LALT_T(KC_BSPC), LT(4, KC_ENTER), LGUI_T(KC_SPACE), RGUI_T(KC_SPACE),  LT(4, KC_ENTER), KC_LBRC, KC_RBRC
+       KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_GRV, LCTL_T(KC_DELETE), LT(4, KC_ENTER), LGUI_T(KC_SPACE), RGUI_T(KC_SPACE),  LT(4, KC_ENTER), KC_LBRC, KC_RBRC
   //                            ╰───────────────────────────╯                           ╰──────────────────╯
   ),
 [LAYER_WIN] = LAYOUT(
@@ -162,9 +160,9 @@ MT(MOD_LCTL | MOD_LGUI | MOD_LALT, KC_TAB),    KC_Q,    KC_W,    KC_E,    KC_R, 
   // ├──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────┤
        KC_LCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   // ├──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────┤
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LGUI_T(KC_DELETE), RGUI_T(KC_DELETE), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGUI_T(KC_DELETE), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   // ╰──────────────────────────────────────────────────────┤                   ├──────────────────────────────────────────────────────╯
-       MO(2), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LT(4, KC_ENTER), LCTL_T(KC_SPACE), RCTL_T(KC_SPACE), LT(4, KC_ENTER), KC_TRNS, KC_TRNS                              
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LGUI_T(KC_DELETE), LT(4, KC_ENTER), LCTL_T(KC_SPACE), RCTL_T(KC_SPACE), LT(4, KC_ENTER), KC_TRNS, KC_TRNS                              
   //                            ╰───────────────────────────╯               ╰──────────────────╯
 ),
   [LAYER_MOUSE] = LAYOUT(
